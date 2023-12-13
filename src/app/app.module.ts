08d8db53a5fb293b57f9cf874e096694772025d1
MalPro <malshis@yahoo.com>
2023-12-13 20:47:47 +0100

    adding router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-
 @NgModule({
-  declarations: [AppComponent, UserProfileComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, MenuComponent, UserProfileComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 // import { Routes } from '@angular/router';
 
 // export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+const routes: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: 'userprofile', component: UserProfileComponent },
+  { path: '', redirectTo: '/userprofile', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
